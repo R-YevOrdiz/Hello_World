8d3ce397ecde0c95eb2e83bf3b5d29a222960366
R-YevOrdiz <yev.ordiz@jmc.edu.ph>
2024-11-05 03:34:12 +0000

    first program

---
+++ Person.java
@@ -0,0 +1,18 @@
+class Person{
+
+    String name;
+    int age;
+    String gender;
+
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
+
+
+
+
+
+
